728ce4e9a6b183b126f7124d22ee75b8befc2cd6
Volonte Rwicha <volonterwicha2004@gmail.com>
2026-04-07 18:43:17 +0200

    remove some imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,4 @@
 import Cart from "./components/Cart";
-import Counter from "./components/Counter";
-import Logger from "./components/Logger";
-import Loop from "./components/Loop";
-import Search from "./components/Search";
 
 function App() {
   return (
